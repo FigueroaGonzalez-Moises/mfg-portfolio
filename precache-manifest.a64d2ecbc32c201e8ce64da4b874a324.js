55404284f1233086593d26ca429800df2989c843
YashiroSennzai <61809595+YashiroSennzai@users.noreply.github.com>
2020-09-02 13:36:27 -0700

    Updates

---
+++ precache-manifest.a64d2ecbc32c201e8ce64da4b874a324.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0471eb15b5585ca8e69f30c52998012a",
+    "url": "/mfg-portfolio/index.html"
+  },
+  {
+    "revision": "fa4f26ce7cc5d7339cc3",
+    "url": "/mfg-portfolio/static/css/main.a603bb5f.chunk.css"
+  },
+  {
+    "revision": "82618f32cd4f537fde4e",
+    "url": "/mfg-portfolio/static/js/2.e9834905.chunk.js"
+  },
+  {
+    "revision": "9a9e56582fbb6743dfc4c1baab3b2d6f",
+    "url": "/mfg-portfolio/static/js/2.e9834905.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa4f26ce7cc5d7339cc3",
+    "url": "/mfg-portfolio/static/js/main.b134bb2b.chunk.js"
+  },
+  {
+    "revision": "e217a5557e11f5db1f35",
+    "url": "/mfg-portfolio/static/js/runtime-main.19513602.js"
+  },
+  {
+    "revision": "85042b4519fc54a9ff9cf238e2642c28",
+    "url": "/mfg-portfolio/static/media/Capture.85042b45.png"
+  }
+]);
